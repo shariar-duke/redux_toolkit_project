7941d357e2b45bfb92305730a5e9927189a6ccb4
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-25 18:50:43 +0600

    index.js file created to run this app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Shariar Duke")
+const store = require("./app/store.js")
+const { fetchVideos } = require("./features/singleVideo/singleVideoSlice.js");
+
+// Subscribe to store updates
+store.subscribe(() => {
+
+});
+
+// Dispatch the thunk action to fetch videos
+store.dispatch(fetchVideos());
